da78a123bf1bd7ed607776a3aa4948c6cea3aa87
Dani Springer <musicbyds@icloud.com>
2018-07-01 03:53:12 +0200

    Create Xcode Project

---
+++ Guess My Number Fun/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Guess My Number Fun
+//
+//  Created by Dani Springer on 01/07/2018.
+//  Copyright © 2018 Dani Springer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
