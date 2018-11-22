ea3ea62e7ad292f79f5a729b2a84e0bf17bdd96e
Dani Springer <musicbyds@icloud.com>
2018-11-22 06:13:21 +0100

    Docs

--- Guess Fun/Controllers/ShuffledExtenstion.swift
+++ Guess Fun/Controllers/ShuffledExtenstion.swift
@@ -11,12 +11,12 @@
 import Foundation
 
 extension MutableCollection {
-    
+
     /// Shuffles the contents of this collection.
     mutating func shuffle() {
         let c = count
         guard c > 1 else { return }
-        
+
         for (firstUnshuffled, unshuffledCount) in zip(indices, stride(from: c, to: 1, by: -1)) {
             // Change `Int` in the next line to `IndexDistance` in < Swift 4.1
             let d: Int = numericCast(arc4random_uniform(numericCast(unshuffledCount)))
@@ -24,17 +24,17 @@ extension MutableCollection {
             swapAt(firstUnshuffled, i)
         }
     }
-    
+
 }
 
 
 extension Sequence {
-    
+
     /// Returns an array with the contents of this sequence, shuffled.
     func shuffled() -> [Element] {
         var result = Array(self)
         result.shuffle()
         return result
     }
-    
+
 }
