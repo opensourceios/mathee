cf4b05d8185f457b2e76cf4c51732b8464ef268e
Dani Springer <musicbyds@icloud.com>
2018-12-30 22:48:24 +0100

    Docs

--- Guess Fun/Controllers/QueensViewController.swift
+++ Guess Fun/Controllers/QueensViewController.swift
@@ -3,7 +3,7 @@
 //  Guess Fun
 //
 //  Created by Daniel Springer on 11/27/18.
-//  Copyright © 2018 Daniel Springer. All rights reserved.
+//  Copyright © 2019 Daniel Springer. All rights reserved.
 //
 
 import UIKit
