c04eef7b292529540fbf9a66aac38a75d0faafb0
Daniel Springer <musicbyds@icloud.com>
2019-07-16 21:12:00 +0200

    fix: add glyphs to menu cells

---
+++ Guess/View/MainMenuTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  MainMenuTableViewCell.swift
+//  Guess
+//
+//  Created by Daniel Springer on 7/16/19.
+//  Copyright © 2019 Dani Springer. All rights reserved.
+//
+
+import UIKit
+
+class MainMenuTableViewCell: UITableViewCell {
+
+    // MARK: Outlets
+
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var myImage: UIImageView!
+
+}
