5a7cf03ee937ab4f6708cccb8ea533e3f4adad60
Daniel Springer <musicbyds@icloud.com>
2020-11-22 22:39:52 -0500

    fixes

--- Guess/Controllers/QueensViewController.swift
+++ Guess/Controllers/QueensViewController.swift
@@ -386,8 +386,7 @@ class QueensViewController: UIViewController {
         """
         let activityController = UIActivityViewController(activityItems: [message], applicationActivities: nil)
         activityController.popoverPresentationController?.barButtonItem = shareButton
-        activityController.completionWithItemsHandler = {
-            (activityType, completed: Bool, returnedItems: [Any]?, error: Error?) in
+        activityController.completionWithItemsHandler = { (_, _: Bool, _: [Any]?, error: Error?) in
             guard error == nil else {
                 let alert = self.createAlert(alertReasonParam: AlertReason.unknown)
                 alert.view.layoutIfNeeded()
