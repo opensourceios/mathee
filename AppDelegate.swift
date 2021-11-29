9b103ff351ed5dee0dabeae6df86bbf0542ac414
Daniel Springer <musicbyds@icloud.com>
2021-11-29 01:58:48 -0500

    fix indentation

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -24,9 +24,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [
-        UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
-
-        return true
-    }
+            UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+                return true
+            }
 
 }
