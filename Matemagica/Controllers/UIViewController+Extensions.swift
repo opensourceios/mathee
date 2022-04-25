9c6e5c4c99f0b71d024e08938a8d3d4aa6c1db25
Daniel Springer <19246409+DaniSpringer@users.noreply.github.com>
2022-04-25 15:28:06 -0400

    add progressbar to formula

--- Matemagica/Controllers/UIViewController+Extensions.swift
+++ Matemagica/Controllers/UIViewController+Extensions.swift
@@ -10,6 +10,8 @@ import UIKit
 
 extension UIViewController {
     func setThemeColorTo(myThemeColor: UIColor) {
+//        UIProgressView.appearance().trackTintColor = .red
+        UIProgressView.appearance().progressTintColor = myThemeColor
         self.navigationController!.navigationBar.tintColor = myThemeColor
         UINavigationBar.appearance().tintColor = myThemeColor
         UIView.appearance(
