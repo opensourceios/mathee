c524dc20f271d8ac33b14a81ffd41682049b558a
Daniel Springer <19246409+DaniSpringer@users.noreply.github.com>
2022-07-24 02:21:59 -0400

    add updown buttons, glow, end screen, more fixes

--- Matemagica/Controllers/HigherLowerViewController.swift
+++ Matemagica/Controllers/HigherLowerViewController.swift
@@ -10,25 +10,23 @@ import UIKit
 
 
 class HigherLowerViewController: UIViewController {
-    // TODO: see all comments in this file, then LATER
 
     // MARK: Outlets
 
     var myTitle: String!
     @IBOutlet weak var guessLabel: UILabel!
     @IBOutlet weak var correctButton: UIButton!
-
+    @IBOutlet weak var lowerButton: UIButton!
+    @IBOutlet weak var higherButton: UIButton!
     @IBOutlet weak var progressBar: UIProgressView!
 
 
     // MARK: Properties
 
-    // LATER:
+    // TODO:
     // let user pick how high the range is, and adjust tries needed accordingly, perhaps a picker showing options
     // for ranges and corresponding needed tries
 
-
-    // TODO: replace swipes with buttons, swipes are not accessible
 
     var myArray = Array(1...1000)
 
@@ -38,16 +36,12 @@ class HigherLowerViewController: UIViewController {
     var guess = 0
 
     var thinkKnow = ""
-    var swipeUpOrDown = ""
-    var swipeUpOrDownAcc = ""
 
     var triedUpperboundOfTwoItemArr = false
 
     let myThemeColor: UIColor = .systemBlue
-    let thinkEmojis = ["🤔", "🤨", "🧐", "🫣", "😅"]
-    let knowEmojis = ["😎", "🤓", "🤗", "😊", "😇"]
-    var swipeUp: UISwipeGestureRecognizer!
-    var swipeDown: UISwipeGestureRecognizer!
+    let thinkEmojis = ["🤔", "🤨", "🧐"]
+    let knowEmojis = ["😎", "🥳", "😏"]
 
 
     // MARK: Life Cycle
@@ -75,7 +69,7 @@ class HigherLowerViewController: UIViewController {
 
         I will guess it in \(triesLeft) tries or less
         """
-
+        toggleButtons(enable: false)
     }
 
 
@@ -83,6 +77,7 @@ class HigherLowerViewController: UIViewController {
         super.viewDidAppear(animated)
 
         progressBar.setProgress(1.0/initialTriesPlusOne, animated: true)
+        correctButton.doGlowAnimation(withColor: myThemeColor)
     }
 
 
@@ -91,34 +86,29 @@ class HigherLowerViewController: UIViewController {
     @objc func start() {
         showNextGuess()
         correctButton.removeTarget(nil, action: nil, for: .allEvents)
-        correctButton.addTarget(self, action: #selector(doneButtonPressed), for: .touchUpInside)
+        correctButton.addTarget(self, action: #selector(correctPressed), for: .touchUpInside)
         correctButton.setTitle(Const.correctMessage, for: .normal)
-        correctButton.isHidden = false
-
-        swipeUp = UISwipeGestureRecognizer(target: self, action: #selector(respondToSwipeGesture))
-        swipeUp.direction = .up
-        self.view.addGestureRecognizer(swipeUp)
-
-        swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(respondToSwipeGesture))
-        swipeDown.direction = .down
-        self.view.addGestureRecognizer(swipeDown)
+        toggleButtons(enable: true)
     }
 
 
     @objc func showNextGuess() {
         triesLeft -= 1
         guard triesLeft >= 0 else {
-            fatalError()
+            let alert = createAlert(alertReasonParam: .unknown)
+            present(alert, animated: true)
+            return
         }
 
-        thinkKnow = "\(thinkEmojis.randomElement()!) I think your number is:"
-        swipeUpOrDown = "Swipe ⬆️ or ⬇️ so I try higher or lower"
-        swipeUpOrDownAcc = "Swipe up or down so I try higher or lower"
+        thinkKnow = """
+        \(thinkEmojis.randomElement()!)
+        I think your number is:
+        """
 
-        if myArray.count == 1 { // WE KNOW
+        if myArray.count == 1 {
             weKnow(localGuess: myArray.first!)
         } else if myArray.count == 2 {
-            if triedUpperboundOfTwoItemArr { // WE KNOW
+            if triedUpperboundOfTwoItemArr {
                 weKnow(localGuess: myArray.last!)
             } else {
                 guess = myArray.first!
@@ -128,7 +118,12 @@ class HigherLowerViewController: UIViewController {
             guess = myArray.middle().first!
         }
 
-        guessLabel.isHidden = false
+        if guess == myArray.first! {
+            lowerButton.isEnabled = false
+        } else if guess == myArray.last! {
+            higherButton.isEnabled = false
+        }
+
         let myAttrString = attrifyString(
             preString: """
 
@@ -140,61 +135,91 @@ class HigherLowerViewController: UIViewController {
 
             """, toAttrify: "\(guess)\n", color: myThemeColor)
         myAttrString.append(NSAttributedString(string: """
-
-        \(swipeUpOrDown)
-
         """))
 
-        guessLabel.attributedText = myAttrString // animate this change
+        guessLabel.attributedText = myAttrString
 
         guessLabel.accessibilityLabel = """
-        Tries left: \(triesLeft). \(thinkKnow): \(guess). \(swipeUpOrDownAcc)
+        Tries left: \(triesLeft). \(thinkKnow): \(guess). Tap Higher, Lower, or Correct.
         """
 
         let myProgress: Float = (initialTriesPlusOne - Float(triesLeft) + 1.0) / initialTriesPlusOne
         progressBar.setProgress(myProgress, animated: true)
     }
 
-
-    func weKnow(localGuess: Int) {
-        self.view.removeGestureRecognizer(swipeUp)
-        self.view.removeGestureRecognizer(swipeDown)
-
-        guess = localGuess
-        thinkKnow = "\(knowEmojis.randomElement()!) I know your number. It is:"
-        swipeUpOrDown = " "
-        swipeUpOrDownAcc = ""
-    }
-
-
-    @objc func respondToSwipeGesture(gesture: UIGestureRecognizer) {
-
-        if let swipeGesture = gesture as? UISwipeGestureRecognizer {
-            switch swipeGesture.direction {
-                case .down:
-                    if guess == myArray.first! {
-                        // user swiped the wrong way
-                        return
-                    }
-                    myArray = Array(myArray.first!...myArray.middle().first!-1)
-                case .up:
-                    if guess == myArray.last! {
-                        // user swiped the wrong way
-                        myArray = Array(myArray.first!...myArray.middle().first!-1)
-                        return
-                    }
-                    myArray = Array(myArray.middle().first!+1...myArray.last!)
-
-                default:
-                    break
+    func toggleButtons(enable: Bool) {
+        for button in [lowerButton, higherButton] {
+            button?.isEnabled = enable
+            if enable {
+                button?.doGlowAnimation(withColor: myThemeColor)
+            } else {
+                button?.layer.removeAnimation(forKey: "shadowGlowingAnimation")
             }
-
-            showNextGuess()
         }
     }
 
 
+    func weKnow(localGuess: Int) {
+        toggleButtons(enable: false)
+        guess = localGuess
+        thinkKnow = """
+        \(knowEmojis.randomElement()!)
+        I know your number. It is:
+        """
+    }
+
+
+    @IBAction func lowerOrHigherTapped(sender: UIButton) {
+        switch sender.tag {
+            case -1: // lower
+                myArray = Array(myArray.first!...myArray.middle().first!-1)
+            case 1: // higher
+                myArray = Array(myArray.middle().first!+1...myArray.last!)
+            default:
+                let alert = createAlert(alertReasonParam: .unknown)
+                present(alert, animated: true)
+        }
+        showNextGuess()
+    }
+
+
     // MARK: Action
+
+    @objc func correctPressed() {
+        correctButton.removeTarget(nil, action: nil, for: .allEvents)
+        correctButton.addTarget(self, action: #selector(doneButtonPressed), for: .touchUpInside)
+        correctButton.setTitle(Const.doneMessage, for: .normal)
+        toggleButtons(enable: false)
+        thinkKnow = """
+        \(knowEmojis.randomElement()!)
+        Knew it all along
+        """
+
+        let myAttrString = attrifyString(
+            preString: """
+
+
+            Tries used: \(Int(initialTriesPlusOne)-1-triesLeft)
+
+            \(thinkKnow)
+
+
+            """, toAttrify: "\(guess)\n", color: myThemeColor)
+        myAttrString.append(NSAttributedString(string: """
+        """))
+
+        guessLabel.attributedText = myAttrString
+
+        guessLabel.accessibilityLabel = """
+        Tries used: \(Int(initialTriesPlusOne)-1-triesLeft).
+        \(thinkKnow).
+        Your number was: \(guess).
+        """
+
+        let myProgress: Float = (initialTriesPlusOne - Float(triesLeft) + 1.0) / initialTriesPlusOne
+        progressBar.setProgress(myProgress, animated: true)
+    }
+
 
     @objc func doneButtonPressed() {
         navigationController?.popToRootViewController(animated: true)
