0e2433720be46b2f474b1c527cd65236205236a8
Daniel Springer <19246409+DaniSpringer@users.noreply.github.com>
2022-10-31 19:08:56 -0400

    reduce book to 5 pages

--- Matemagica/Controllers/BookViewController.swift
+++ Matemagica/Controllers/BookViewController.swift
@@ -34,40 +34,19 @@ class BookViewController: UIViewController, UICollectionViewDelegate, UICollecti
     let pagesArrayOfDicts = [
         1: [1, 3, 5, 7, 9, 11,
             13, 15, 17, 19, 21, 23,
-            25, 27, 29, 31, 33, 35,
-            37, 39, 41, 43, 45, 47,
-            49, 51, 53, 55, 57, 59,
-            61, 63],
+            25, 27, 29, 31],
         2: [2, 3, 6, 7, 10, 11,
             14, 15, 18, 19, 22, 23,
-            26, 27, 30, 31, 34, 35,
-            38, 39, 42, 43, 46, 47,
-            50, 51, 54, 55, 58, 59,
-            62, 63],
+            26, 27, 30, 31],
         4: [4, 5, 6, 7, 12, 13,
             14, 15, 20, 21, 22, 23,
-            28, 29, 30, 31, 36, 37,
-            38, 39, 44, 45, 46, 47,
-            52, 53, 54, 55, 60, 61,
-            62, 63],
+            28, 29, 30, 31],
         8: [8, 9, 10, 11, 12, 13,
             14, 15, 24, 25, 26, 27,
-            28, 29, 30, 31, 40, 41,
-            42, 43, 44, 45, 46, 47,
-            56, 57, 58, 59, 60, 61,
-            62, 63],
+            28, 29, 30, 31],
         16: [16, 17, 18, 19, 20, 21,
              22, 23, 24, 25, 26, 27,
-             28, 29, 30, 31, 48, 49,
-             50, 51, 52, 53, 54, 55,
-             56, 57, 58, 59, 60, 61,
-             62, 63],
-        32: [32, 33, 34, 35, 36, 37,
-             38, 39, 40, 41, 42, 43,
-             44, 45, 46, 47, 48, 49,
-             50, 51, 52, 53, 54, 55,
-             56, 57, 58, 59, 60, 61,
-             62, 63]]
+             28, 29, 30, 31]]
 
     var arrayOfPages = [Page]()
     var shuffledPagesByContent = [Page]()
@@ -116,9 +95,9 @@ class BookViewController: UIViewController, UICollectionViewDelegate, UICollecti
         shuffledPagesByOrder = shuffledPagesByContent.shuffled()
 
         pageNumberLabel.text = """
-        Think of a number from 1 to 63
+        Think of a number from 1 to 31
 
-        Tell me whether you spot your number in the following 6 lists
+        Tell me whether you spot your number in the following \(pagesArrayOfDicts.count) lists
         """
         myCollectionView.isHidden = true
 
