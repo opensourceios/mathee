33c0b99425ca9a86d52daaff373ada1818798153
Daniel Springer <19246409+DaniSpringer@users.noreply.github.com>
2022-12-25 18:31:03 -0500

    fixes

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -13,6 +13,18 @@ import UIKit
 
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    // TODO: Shabbos
+    // - Tell me which I got wrong on level ended?
+    // - Show me next level's range
+    // - Allow changing base from 7 to 2-9 (or even higher?)
+    // - Allow changing lives per level? (min 1 or infinite option?)
+    // - Allow changing goal? (min 1x correct guess value)
+    // - Allow changing timer?
+    // - Allow changing added values to each level?
+    // - Allow changing points per guess?
+    // - Allow changing...?
+    // - Update tutorial to reflect customizable values
+    // - Update single levels UI to reflect customizable values
 
     // MARK: Properties
 
